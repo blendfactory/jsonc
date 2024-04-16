923a05f1171be7e1953895ec89ecbc2ea8fd775e
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-04-16 23:38:47 +0900

    refactor: use final for local variable declarations in examples
    Modify the example Dart files for both the jsonc_parser and jsonc_scanner packages to use `final` instead of `var` for the `awesome` variable declaration. This change promotes best practices in immutability where the variable value is not expected to change.

--- packages/jsonc_parser/example/jsonc_parser_example.dart
+++ packages/jsonc_parser/example/jsonc_parser_example.dart
@@ -1,6 +1,6 @@
 import 'package:jsonc_parser/jsonc_parser.dart';
 
 void main() {
-  var awesome = Awesome();
+  final awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
 }
--- packages/jsonc_scanner/example/jsonc_scanner_example.dart
+++ packages/jsonc_scanner/example/jsonc_scanner_example.dart
@@ -1,6 +1,6 @@
 import 'package:jsonc_scanner/jsonc_scanner.dart';
 
 void main() {
-  var awesome = Awesome();
+  final awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
 }
