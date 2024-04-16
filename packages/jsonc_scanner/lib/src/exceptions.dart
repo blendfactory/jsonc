7df91eaa3a82b977f9d94b2f6c0ee6a271f0a2d1
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-04-16 23:38:39 +0900

    style: standardize quotes in exception messages
    Update all exception messages in the `jsonc_scanner` package to use single quotes instead of double quotes. This change standardizes the string literals throughout the exceptions file, aligning with Dart style guidelines.

--- packages/jsonc_scanner/lib/src/exceptions.dart
+++ packages/jsonc_scanner/lib/src/exceptions.dart
@@ -4,29 +4,29 @@ sealed class ScanException implements Exception {
   final String message;
 
   @override
-  String toString() => "ScanException: $message";
+  String toString() => 'ScanException: $message';
 }
 
 final class UnexpectedEndOfComment extends ScanException {
-  const UnexpectedEndOfComment() : super("Unexpected end of comment");
+  const UnexpectedEndOfComment() : super('Unexpected end of comment');
 }
 
 final class UnexpectedEndOfString extends ScanException {
-  const UnexpectedEndOfString() : super("Unexpected end of string");
+  const UnexpectedEndOfString() : super('Unexpected end of string');
 }
 
 final class UnexpectedEndOfNumber extends ScanException {
-  const UnexpectedEndOfNumber() : super("Unexpected end of number");
+  const UnexpectedEndOfNumber() : super('Unexpected end of number');
 }
 
 final class InvalidUnicode extends ScanException {
-  const InvalidUnicode() : super("Invalid Unicode escape sequence");
+  const InvalidUnicode() : super('Invalid Unicode escape sequence');
 }
 
 final class InvalidEscapeCharacter extends ScanException {
-  const InvalidEscapeCharacter() : super("Invalid escape character");
+  const InvalidEscapeCharacter() : super('Invalid escape character');
 }
 
 final class InvalidCharacter extends ScanException {
-  const InvalidCharacter() : super("Invalid character");
+  const InvalidCharacter() : super('Invalid character');
 }
