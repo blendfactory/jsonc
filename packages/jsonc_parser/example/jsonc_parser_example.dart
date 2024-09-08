319973184beb93e8cee9537fbf23b1e3e965423c
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-09-08 12:30:56 +0900

    chore: Add ignore for sample code print statements

--- packages/jsonc_parser/example/jsonc_parser_example.dart
+++ packages/jsonc_parser/example/jsonc_parser_example.dart
@@ -2,5 +2,7 @@ import 'package:jsonc_parser/jsonc_parser.dart';
 
 void main() {
   final awesome = Awesome();
+  // Because this is sample code.
+  // ignore: avoid_print
   print('awesome: ${awesome.isAwesome}');
 }
--- packages/jsonc_scanner/example/jsonc_scanner_example.dart
+++ packages/jsonc_scanner/example/jsonc_scanner_example.dart
@@ -2,5 +2,7 @@ import 'package:jsonc_scanner/jsonc_scanner.dart';
 
 void main() {
   final awesome = Awesome();
+  // Because this is sample code.
+  // ignore: avoid_print
   print('awesome: ${awesome.isAwesome}');
 }
